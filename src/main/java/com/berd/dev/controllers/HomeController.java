9050392db80ada3cf6b492208150ae5dcdb71073
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-03-06 21:09:58 +0300

    Update HomeController.java

--- src/main/java/com/berd/dev/controllers/HomeController.java
+++ src/main/java/com/berd/dev/controllers/HomeController.java
@@ -3,7 +3,6 @@ package com.berd.dev.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.berd.dev.services.UniteService;
 
