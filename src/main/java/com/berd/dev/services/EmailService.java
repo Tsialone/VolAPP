d8201dfbe947e3cce301f8a7d1e290e9cd1676f4
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-03-16 22:47:49 +0300

    add Async call in emailService

--- src/main/java/com/berd/dev/services/EmailService.java
+++ src/main/java/com/berd/dev/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.berd.dev.services;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,19 @@ public class EmailService {
 
     private  final JavaMailSender mailSender; // Spring l'injecte grâce aux properties ci-dessus
 
+    @Async
     public void envoyerEmail(String vers, String sujet, String contenu) {
-        SimpleMailMessage message = new SimpleMailMessage();
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(vers);
         message.setSubject(sujet);
         message.setText(contenu);
         mailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
+        
     }
 }
 
