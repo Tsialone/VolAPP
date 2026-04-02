25499dd8fe3563e0bfadb861bd4ea5a208eea1fa
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-04-02 15:39:33 +0300

    repositories for mvtCaisse

---
+++ src/main/java/com/berd/dev/repositories/CaisseMvtRepository.java
@@ -0,0 +1,9 @@
+package com.berd.dev.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.berd.dev.models.CaisseMvt;
+
+public interface CaisseMvtRepository extends JpaRepository<CaisseMvt, Integer> {
+
+}
--- src/main/java/com/berd/dev/repositories/DepenseRepository.java
+++ src/main/java/com/berd/dev/repositories/DepenseRepository.java
@@ -4,10 +4,18 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.berd.dev.models.Depense;
 
 public interface DepenseRepository extends JpaRepository<Depense, Integer>, JpaSpecificationExecutor<Depense> {
     List<Depense> findByUtilisateurIdUtilisateur(Long idUtilisateur);
+
+    @Query("SELECT DISTINCT d FROM depense d JOIN FETCH d.depenseDetails")
+    List<Depense> findAllWithDepenseDetails();
+
+    @Query("SELECT DISTINCT d FROM depense d JOIN FETCH d.depenseDetails WHERE d.idDepense = :id_depense")
+    Depense findAllWithDepenseDetailsByIdDepense(@Param("id_depense") Integer idDepense);
 
 }
