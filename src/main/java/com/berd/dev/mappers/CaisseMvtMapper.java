6ec833e94bd866520775066292f6c3cda16c075c
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-04-03 16:31:03 +0300

    create CaisseMapper for CaisseMvt

---
+++ src/main/java/com/berd/dev/mappers/CaisseMvtMapper.java
@@ -0,0 +1,28 @@
+package com.berd.dev.mappers;
+
+import java.util.List;
+
+import com.berd.dev.dtos.CaisseMvtDto;
+import com.berd.dev.models.CaisseMvt;
+
+public class CaisseMvtMapper {
+    public static CaisseMvtDto toDto (CaisseMvt caisseMvt) {
+        if (caisseMvt == null) return null;
+
+        CaisseMvtDto resp = new CaisseMvtDto();
+        resp.setIdCaisse(caisseMvt.getCaisse().getIdCaisse());
+        resp.setNomCaisse(caisseMvt.getCaisse().getNom());
+        resp.setMontant(caisseMvt.getMontant());
+        resp.setType(caisseMvt.getType());
+        resp.setDateHeureTransaction(caisseMvt.getCreated());
+
+        if (caisseMvt.getDepense() != null) {
+            resp.setNomDepense(caisseMvt.getDepense().getDescription());
+        }
+
+        return resp;
+    }
+    public static List<CaisseMvtDto> tDtos (List<CaisseMvt> caisseMvts) {
+        return caisseMvts.stream().map(CaisseMvtMapper::toDto).toList();
+    }
+}
